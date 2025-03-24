68ae7ccd4a8ac67a3c2b0b2280224071113e5d5b
Naveen Singh <naveenpundir882@gmail.com>
2025-03-24 10:13:33 +0530

    basicloops.c

---
+++ basicloops.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // int n;
+    // printf("Enter a number : ");
+    // scanf("%d",&n);
+   for(int i=1; i<=10; i=i+4){
+    printf("Life is beautiful : \n");
+}
+    return 0;
+}
