04654ea59182324d51433cd81bcd92629301462b
Naveen Singh <naveenpundir882@gmail.com>
2025-03-28 10:38:19 +0530

    switch.c

---
+++ switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int i = 1;
+    
+    // Using swith statement
+	switch (i) {
+	case 1:
+		printf("Case 1");
+		break;
+	case 2:
+		printf("Case 2");
+		break;
+	case 3:
+		printf("Case 3");
+		break;
+	case 4:
+		printf("Case 4");
+		break;
+	default:
+		printf("Default");
+		break;
+	}
+  return 0;
+}
